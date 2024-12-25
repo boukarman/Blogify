01986116b17ed49ea1b4f29236b9eff682906d66
Naoufel Boukarma <boukarman@yahoo.fr>
2024-12-26 00:16:36 +0100

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,56 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">MyBlog</a>
+        <nav> 
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <div className="image">
+          <img src="https://www.wscubetech.com/blog/wp-content/uploads/2024/04/generative-ai.webp" alt="" />
+        </div>
+        <div className="texts">
+          <h2>Generative AI</h2>
+          <p className="info">
+            <a className="author">Naoufel Boukarma</a>
+            <time>24.12.2024 00:33</time>
+          </p>
+          <p className="summary">You might not recognize Generative AI by name, but many of you might use it daily. Almost every industry is relying on it to generate compelling and informational content. It uses artificial intelligence to create fresh content using given prompts and information. One famous example of generative AI is ChatGPT, a buzzword in the market that many aspirants are learning to gain better professional opportunities.</p>
+        </div>
+      </div>   
+      <div className="post">
+        <div className="image">
+          <img src="https://www.wscubetech.com/blog/wp-content/uploads/2024/04/generative-ai.webp" alt="" />
+        </div>
+        <div className="texts">
+          <h2>Generative AI</h2>
+          <p className="info">
+            <a className="author">Naoufel Boukarma</a>
+            <time>24.12.2024 00:33</time>
+          </p>
+          <p className="summary">You might not recognize Generative AI by name, but many of you might use it daily. Almost every industry is relying on it to generate compelling and informational content. It uses artificial intelligence to create fresh content using given prompts and information. One famous example of generative AI is ChatGPT, a buzzword in the market that many aspirants are learning to gain better professional opportunities.</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image"> 
+          <img src="https://www.wscubetech.com/blog/wp-content/uploads/2024/04/generative-ai.webp" alt="" />
+        </div>
+        <div className="texts">
+          <h2>Generative AI</h2>
+          <p className="info">
+            <a className="author">Naoufel Boukarma</a>
+            <time>24.12.2024 00:33</time>
+          </p>
+          <p className="summary">You might not recognize Generative AI by name, but many of you might use it daily. Almost every industry is relying on it to generate compelling and informational content. It uses artificial intelligence to create fresh content using given prompts and information. One famous example of generative AI is ChatGPT, a buzzword in the market that many aspirants are learning to gain better professional opportunities.</p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;
