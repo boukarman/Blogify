09b6c84aa5fadeb923a6804d78de9195a5ec323a
Naoufel Boukarma <boukarman@yahoo.fr>
2025-01-20 11:10:51 +0100

    Added Post and User controllers

--- server/controllers/postControllers.js
+++ server/controllers/postControllers.js
@@ -1,27 +1,212 @@
+const Post = require('../models/postModel');
+const User = require('../models/userModel');
+const path = require('path');
+const fs = require('fs');
+const { v4: uuid } = require('uuid');
+const HttpError = require('../models/errorModel');
+
+
+
+/****************Create a post
+ * post: api/posts
+ * UNPROTECTED
+ */
 const createPost = async (req, res, next) => {
-    // Logic for creating a post will be added here.
-  };
-  
-  const getPosts = async (req, res, next) => {
-    // Logic for fetching all posts will be added here.
-  };
-  
-  const getPost = async (req, res, next) => {
-    // Logic for fetching a single post will be added here.
-  };
-  
-  const editPost = async (req, res, next) => {
-    // Logic for editing a post will be added here.
-  };
-  
-  const deletePost = async (req, res, next) => {
-    // Logic for deleting a post will be added here.
-  };
-  
-  module.exports = {
-    createPost,
-    getPosts,
-    getPost,
-    editPost,
-    deletePost,
-  };
+	try {
+		let { title, category, description } = req.body;
+		if (!title || !category || !description) {
+			return next(new HttpError('Please fill all the fields.', 422));
+		}
+
+		if (!req?.files?.thumbnail) {
+			return next(new HttpError('Please choose and image.', 422));
+		}
+		const { thumbnail } = req.files;
+		if (thumbnail.size > 2000000) {
+			return next(new HttpError('Thumbnail is too big. Should be less then 2mb.', 422));
+		}
+		let fileName = thumbnail.name;
+		let splittedFileName = fileName.split('.');
+		let newFileName = splittedFileName[0] + '-' + uuid() + '.' + splittedFileName[splittedFileName.length - 1];
+
+		await thumbnail.mv(path.join(__dirname, '..', 'uploads', newFileName), async (error) => {
+			if (error) {
+				return next(new HttpError(error));
+			}
+			const newPost = await Post.create({ title, category, description, thumbnail: newFileName, creator: req.user.id });
+			if (!newPost) {
+				return next(new HttpError('Failed to create post.', 422));
+			}
+			// find user and increase post count by 1
+			const currentUser = await User.findById(req.user.id);
+			const userPostCount = currentUser.posts + 1
+			await User.findByIdAndUpdate(req.user.id, { posts: userPostCount });
+			res.status(201).json({ newPost });
+		})
+	} catch (error) {
+		return next(new HttpError(error.message));
+	}
+}
+
+/****************Get a post
+ * get: api/posts
+ * UNPROTECTED
+ */
+const getPosts = async (req, res, next) => {
+	try {
+		const posts = await Post.find().sort({ createdAt: -1 });
+		res.status(200).json(posts);
+
+	} catch (error) {
+		return next(new HttpError(error));
+	}
+}
+
+/****************Get a post
+ * get: api/posts/:id
+ * UNPROTECTED
+ */
+const getPost = async (req, res, next) => {
+	try {
+		const { id } = req.params;
+		const post = await Post.findById(id);
+		if (!post) {
+			return next(new HttpError('Post not found.', 404))
+		}
+		res.status(200).json(post);
+	} catch (error) {
+		// console.log(error)
+		return next(new HttpError(error, error.status));
+	}
+}
+
+/****************Get posts by category
+ * get: api/posts/categories/:category
+ * UNPROTECTED
+ */
+const getPostByCategory = async (req, res, next) => {
+	try {
+		const { category } = req.params;
+		const catPosts = await Post.find({ category: category }).sort({ createdAt: -1 });
+		if (!catPosts) {
+			return next(new HttpError('Posts not found.', 404))
+		}
+		res.status(200).json(catPosts);
+	} catch (error) {
+		return next(new HttpError(error));
+	}
+}
+
+/****************Get posts by author
+ * get: api/posts/users/:id
+ * UNPROTECTED
+ */
+const getUserPosts = async (req, res, next) => {
+	try {
+		const { id } = req.params;
+		const userPosts = await Post.find({ creator: id }).sort({ createdAt: -1 });
+		if (!userPosts) {
+			return next(new HttpError('Posts not found.', 404))
+		}
+		res.status(200).json(userPosts);
+	} catch (error) {
+		return next(new HttpError(error));
+	}
+}
+
+/****************Edit Post
+ * patch: api/posts/:id
+ * PROTECTED
+ */
+const editPost = async (req, res, next) => {
+	try {
+		let fileName;
+		let newFileName;
+		let updatedPost;
+		const postId = req.params.id;
+		let { title, category, description } = req.body;
+		if (!title || !category || !description) {
+			return next(new HttpError('Please fill all the fields.', 422));
+		}
+		const oldPost = await Post.findById(postId);
+		// console.log(oldPost.creator.toString(), req.user.id)
+		if (oldPost.creator.toString() !== req.user.id) {
+			return next(new HttpError('You are not allowed to edit this post.', 403));
+		}
+		if (!req?.files) {
+			updatedPost = await Post.findByIdAndUpdate(postId, { title, category, description }, { new: true });
+		} else {
+			// get old post
+
+			fs.unlink(path.join(__dirname, '..', 'uploads', oldPost.thumbnail), async (error) => {
+				if (error) {
+					return next(new HttpError(error));
+				}
+			})
+			const { thumbnail } = req.files;
+			if (thumbnail.size > 2000000) {
+				return next(new HttpError('Thumbnail is too big. Should be less then 2mb.', 422));
+			}
+			fileName = thumbnail.name;
+			const splittedFileName = fileName.split('.');
+			newFileName = splittedFileName[0] + '-' + uuid() + '.' + splittedFileName[splittedFileName.length - 1];
+			await thumbnail.mv(path.join(__dirname, '..', 'uploads', newFileName), async (error) => {
+				if (error) {
+					return next(new HttpError(error));
+				}
+			})
+			updatedPost = await Post.findByIdAndUpdate(postId, { title, category, description, thumbnail: newFileName }, { new: true });
+			if (!updatedPost) {
+				return next(new HttpError('Failed to update post.', 422));
+			}
+		}
+		res.status(200).json(updatedPost);
+	} catch (error) {
+		return next(new HttpError(error));
+	}
+}
+
+/****************delete Post
+ * delete: api/posts/:id
+ * PROTECTED
+ */
+const deletePost = async (req, res, next) => {
+	try {
+		const postId = req.params.id;
+		// console.log(postId)
+
+		const post = await Post.findById(postId);
+
+		if (!post) {
+			return next(new HttpError('Post not found.', 404));
+		}
+		if (post.creator.toString() !== req.user.id) {
+			return next(new HttpError('You are not allowed to delete this post.', 403));
+		}
+		const fileName = post.thumbnail;
+		fs.unlink(path.join(__dirname, '..', 'uploads', fileName), async (error) => {
+			if (error) {
+				return next(new HttpError(error));
+			}
+		})
+		await Post.findByIdAndDelete(postId);
+		// find user and reduce post count by 1
+		const currentUser = await User.findById(post.creator);
+		const userPostCount = currentUser.posts - 1;
+		await User.findByIdAndUpdate(post.creator, { posts: userPostCount }, { new: true });
+		res.status(200).json({ message: 'Post deleted successfully.' });
+	} catch (error) {
+
+	}
+}
+
+
+module.exports = {
+	createPost,
+	getPosts,
+	getPost,
+	getPostByCategory,
+	getUserPosts,
+	editPost,
+	deletePost
+}
--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,17 +1,251 @@
+const HttpError = require('../models/errorModel');
+const User = require('../models/userModel');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
+const path = require('path');
+const { v4: uuid } = require('uuid');
+
+/*************Register a new User */
+// POST : api/users/register
+// UNPROTECTED
 const registerUser = async (req, res, next) => {
-    // Logic for registering a user will be added here.
-  };
-  
-  const loginUser = async (req, res, next) => {
-    // Logic for logging in a user will be added here.
-  };
-  
-  const getUser = async (req, res, next) => {
-    // Logic for fetching a user's profile will be added here.
-  };
-  
-  module.exports = {
-    registerUser,
-    loginUser,
-    getUser,
-  };
+	try {
+		const { name, email, password, password2 } = req.body;
+		if (!name || !email || !password || !password2) {
+			return next(new HttpError('Fill in all fields.', 422))
+		}
+		const newEmail = email.toLowerCase();
+
+		const emailExists = await User.findOne({ email: newEmail })
+
+		if (emailExists) {
+			return next(new HttpError('Email already exists.', 422))
+		}
+
+		if ((password.trim().length < 6)) {
+			return next(new HttpError('Password must be at least 6 characters.', 422));
+		}
+
+		if (password !== password2) {
+			return next(new HttpError('Passwords do not match.', 422));
+		}
+
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(password, salt);
+		const avatar = "avatar13.jpg";
+		const newUser = await User.create({ name, email: newEmail, password: hashedPassword, avatar});
+
+		res.status(201).json({ message: `New user ${newUser.email} registered successfully.` });
+	} catch (error) {
+		next(new HttpError('User registration failed.', 422))
+	}
+}
+
+
+
+
+
+
+
+
+
+
+/*************Login a registered user*/
+// POST : api/users/login
+// UNPROTECTED
+
+const loginUser = async (req, res, next) => {
+	try {
+		const { email, password } = req.body;
+		if (!email || !password) {
+			return next(new HttpError('Fill in all fields.', 422));
+		}
+
+		const newEmail = email.toLowerCase();
+
+		const user = await User.findOne({ email: newEmail });
+
+		if (!user) {
+			return next(new HttpError('Invalid credentials.', 422));
+		}
+
+
+		const comparePass = await bcrypt.compare(password, user.password);
+
+		if (!comparePass) {
+			return next(new HttpError('Invalid credentials.', 422));
+		}
+
+		const { _id: id, name } = user;
+
+		const token = jwt.sign({ id, name }, process.env.JWT_SECRET, { expiresIn: '1d' });
+
+		res.status(200).json({ token, id, name, email });
+
+	} catch (error) {
+		return next(new HttpError('Login failed. Please check your credentials.', 422));
+	}
+}
+
+
+
+
+
+
+
+
+/*************User profile */
+// GET : api/users/:id
+// PROTECTED
+const getUser = async (req, res, next) => {
+	try {
+		const { id } = req.params;
+		const user = await User.findById(id).select('-password');
+		if (!user) {
+			return next(new HttpError('User not found.', 404))
+		}
+		res.status(200).json(user);
+
+	} catch (error) {
+		return next(new HttpError(`Failed to find user with id ${req.params.id}`, 422));
+	}
+}
+
+
+
+
+
+
+
+/*************Change user avatar */
+// POST : api/users/change-avatar
+// PROTECTED
+const changeAvatar = async (req, res, next) => {
+	try {
+		console.log(req.files)
+		if (!req?.files?.avatar) {
+			return next(new HttpError('Please choose and image.', 422));
+		}
+		//find user from database
+		const user = await User.findById(req.user.id);
+		// delete hole avatar if exists
+		if (user?.avatar) {
+			fs.unlink(path.join(__dirname, '..', 'uploads', user.avatar), (err) => {
+				if (err) {
+					return next(new HttpError(err));
+				}
+			})
+		}
+
+		const { avatar } = req.files;
+		// check file size
+		if (avatar.size > 500000) {
+			return next(new HttpError('Profile picture is too big. Should be less then 500kb.', 422));
+		}
+
+		let fileName = avatar.name;
+		let splittedFileName = fileName.split('.');
+		let newFileName = splittedFileName[0] + '-' + uuid() + '.' + splittedFileName[splittedFileName.length - 1];
+
+		await avatar.mv(path.join(__dirname, '..', 'uploads', newFileName), async (error) => {
+			if (error) {
+				return next(new HttpError(error));
+			}
+			const updatedAvatar = await User.findByIdAndUpdate(req.user.id, { avatar: newFileName }, { new: true });
+			if (!updatedAvatar) {
+				return next(new HttpError('Avatar could not be updated.', 422));
+			}
+			res.status(200).json(updatedAvatar);
+		});
+
+
+	} catch (error) {
+		return next(new HttpError(error));
+	}
+}
+
+
+
+
+
+
+
+
+/*************Edi user details */
+// POST : api/users/edit-user
+// PROTECTED
+const editUser = async (req, res, next) => {
+	try {
+		const { name, email, currentPassword, newPassword, newPassword2 } = req.body;
+		if (!name || !email || !currentPassword || !newPassword || !newPassword2) {
+			return next(new HttpError('Fill in all fields.', 422));
+		}
+
+		// get user from database
+		const user = await User.findById(req.user.id);
+		if (!user) {
+			return next(new HttpError('User not found.', 404));
+		}
+
+		// make sure new email doesnt already exist
+		const emailExist = await User.findOne({ email });
+		// we want to update other details with/without changing the email (which is a unique id because we use it to login)
+		if (emailExist && (emailExist._id != req.user.id)) {
+			return next(new HttpError('Email already exists.', 422));
+		}
+
+		// compare current password to db password
+		const validateUserPassword = await bcrypt.compare(currentPassword, user.password);
+		if (!validateUserPassword) {
+			return next(new HttpError('Invalid current password.', 422));
+		}
+
+		// compare new password with new password2
+		if (newPassword !== newPassword2) {
+			return next(new HttpError('Passwords do not match.', 422));
+		}
+
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(newPassword, salt);
+
+		// update user infor in database
+
+		const newInfo = await User.findByIdAndUpdate(req.user.id, { name, email, password: hashedPassword }, { new: true });
+
+		res.status(200).json(newInfo);
+	} catch (error) {
+		console.log(error);
+		return next(new HttpError(error));
+	}
+}
+
+
+
+
+
+
+
+/*************Edi Authors */
+// GET : api/users/getAuthors
+// UNPROTECTED
+const getAuthors = async (req, res, next) => {
+	try {
+		const authors = await User.find().select('-password');
+		if (!authors) {
+			return next(new HttpError('No authors found.', 404));
+		}
+		res.status(200).json(authors);
+	} catch (error) {
+		return next(new HttpError(error));
+	}
+}
+
+module.exports = {
+	registerUser,
+	loginUser,
+	getUser,
+	changeAvatar,
+	editUser,
+	getAuthors
+}
